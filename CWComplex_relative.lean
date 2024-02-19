c3bab5316be224dc1f66aa8d8c4e5ad837dcf797
jzxia <xiajiazhen@gmail.com>
2024-02-19 14:54:49 +0800

    relative CWComplex (draft, failed to prove termination)

--- CWComplex_relative.lean
+++ CWComplex_relative.lean
@@ -17,6 +17,13 @@ import Mathlib.Init.Set
 
 open CategoryTheory
 
+namespace tmp1
+  set_option trace.Meta.synthInstance true
+  #check (TopCat.of Empty)
+  #check ∅
+  --#check (TopCat.of ∅)
+end tmp1
+
 
 namespace CWComplex
 noncomputable section
@@ -45,10 +52,87 @@ theorem continuous_SphereInclusion (n : ℤ) : Continuous (SphereInclusion n) :=
       use t, ht
       rw [ht'.symm]
       tauto⟩
-  | Int.negSucc n => ⟨by
-      dsimp [SphereInclusion, ClosedBall]
-      intro s hs
-      sorry⟩
+  | Int.negSucc n => ⟨by tauto⟩
+
+def SigmaSphereInclusion (n : ℤ) (cells : Type) :
+    (Σ (_ : cells), 𝕊 n) → (Σ (_ : cells), 𝔻 n + 1) :=
+  Sigma.map id fun _ x => SphereInclusion n x
+
+theorem continuous_sigmaSphereInclusion (n : ℤ) (cells : Type) :
+    Continuous (SigmaSphereInclusion n cells) := by
+  apply Continuous.sigma_map
+  intro _
+  apply continuous_SphereInclusion
+
+def BundledSigmaSphereInclusion (n : ℤ) (cells : Type) :
+    ContinuousMap (TopCat.of (Σ (_ : cells), 𝕊 n)) (TopCat.of (Σ (_ : cells), 𝔻 n + 1)) :=
+  ⟨SigmaSphereInclusion n cells, continuous_sigmaSphereInclusion n cells⟩
+
+def SigmaAttachMap (X : TopCat) (n : ℤ) (cells : Type)
+    (attach_maps : cells → ContinuousMap (𝕊 n) X) :
+    (Σ (_ : cells), 𝕊 n) → X :=
+  fun ⟨i, x⟩ => attach_maps i x
+
+theorem continuous_sigmaAttachMap (X : TopCat) (n : ℤ) (cells : Type)
+    (attach_maps : cells → ContinuousMap (𝕊 n) X) :
+    Continuous (SigmaAttachMap X n cells attach_maps) := by
+  apply continuous_sigma
+  exact fun i => (attach_maps i).continuous_toFun
+
+def BundledSigmaAttachMap (X : TopCat) (n : ℤ) (cells : Type)
+    (attach_maps : cells → ContinuousMap (𝕊 n) X) :
+    ContinuousMap (TopCat.of (Σ (_ : cells), 𝕊 n)) X :=
+  ⟨SigmaAttachMap X n cells attach_maps, continuous_sigmaAttachMap X n cells attach_maps⟩
+
+-- A type witnessing that X' is obtained from X by attaching n-cells
+structure AttachCells (X X' : TopCat) (n : ℤ) where
+  /- The index type over n-cells -/
+  cells : Type
+  attach_maps : cells → ContinuousMap (𝕊 n) X
+  iso_pushout : X' ≅ Limits.pushout
+    (BundledSigmaSphereInclusion n cells)
+    (BundledSigmaAttachMap X n cells attach_maps)
+
+end
+end CWComplex
+
+
+
+structure CWComplex where
+  /- Skeleta -/
+  sk : ℤ → TopCat
+  /- Every n-skeleton for n < 0 is empty. -/
+  sk_neg_empty : ∀ n < 0, sk n = Empty
+  /- The (n + 1)-skeleton is obtained from the n-skeleton by attaching (n + 1)-cells. -/
+  attach_cells : (n : ℤ) → CWComplex.AttachCells (sk n) (sk (n + 1)) (n + 1)
+
+
+
+namespace CWComplex
+noncomputable section
+
+-- The inclusion map from X to X', given that X' is obtained from X by attaching n-cells
+def AttachCellsInclusion (X X' : TopCat) (n : ℤ) (att : AttachCells X X' n) : X ⟶ X'
+  := @Limits.pushout.inr TopCat _ _ _ X
+      (BundledSigmaSphereInclusion n att.cells)
+      (BundledSigmaAttachMap X n att.cells att.attach_maps) _ ≫ att.iso_pushout.inv
+
+-- The inclusion map from the n-skeleton to the (n+1)-skeleton of a CW-complex
+def SkeletaInclusion (X : CWComplex) (n : ℤ) : X.sk n ⟶ X.sk (n + 1) :=
+  AttachCellsInclusion (X.sk n) (X.sk (n + 1)) (n + 1) (X.attach_cells n)
+
+-- The inclusion map from the n-skeleton to the m-skeleton of a CW-complex
+-- Note: A dependently-typed `List` with `List.range'` and `List.foldl_assoc` could help here.
+-- Does mathlib have that?
+def SkeletaInclusion' (X : CWComplex) (n : ℤ) (m : ℤ) (n_le_m : n ≤ m) :
+    X.sk n ⟶ X.sk m :=
+  if h : n = m then by
+    rw [<- h]
+    exact 𝟙 (X.sk n)
+  else by
+    have : n < m := Int.lt_iff_le_and_ne.mpr ⟨n_le_m, h⟩
+    exact SkeletaInclusion X n ≫ SkeletaInclusion' X (n + 1) m this
+  termination_by m - n
 
 end
 end CWComplex
