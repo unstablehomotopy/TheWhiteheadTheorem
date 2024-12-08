375ca2038e95101cb7a085d508c80d2256db39e1
jzxia <xiajiazhen@gmail.com>
2024-12-08 23:39:51 +0800

    update mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -4,22 +4,22 @@ open Lake DSL
 package «threeWhiteheadTheoremsandThreePuppeSequences» {
   -- add any package configuration options here
 
-    moreLinkArgs := #[
-    "-L./.lake/packages/LeanCopilot/.lake/build/lib", -- LeanCopilot
-    "-lctranslate2"                                   -- LeanCopilot
-  ]
+  --   moreLinkArgs := #[
+  --   "-L./.lake/packages/LeanCopilot/.lake/build/lib", -- LeanCopilot
+  --   "-lctranslate2"                                   -- LeanCopilot
+  -- ]
 }
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
-require batteries from git
-  "https://github.com/leanprover-community/batteries" @ "main"
+-- require batteries from git
+--   "https://github.com/leanprover-community/batteries" @ "main"
 
-require LeanCopilot from git  -- LeanCopilot
-  "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.4.1"
+-- require LeanCopilot from git  -- LeanCopilot
+--   "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.4.1"
 
 @[default_target]
-lean_lib «CWComplex» {
+lean_lib «CWComplex/CWComplex» {
   -- add any library configuration options here
 }
